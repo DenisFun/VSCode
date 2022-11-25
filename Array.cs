1ce60857f413fe2f2894b1fa04c4271d82ae17d3
Denis <d.afanasyev@xlab-game.com>
2022-11-25 10:09:39 +0300

    Add_Sort

--- Array.cs
+++ Array.cs
@@ -1,51 +1,149 @@
 ﻿using System;
 using System.Collections;
+using System.Diagnostics;
 
 class Arrays
 {
     static void Main()
     {
-        MyLinkedList<int> list = new MyLinkedList<int>();
-		for (int i = 0; i < 10; i++)
-			list.Add(i);
+		Stopwatch stopwatch = new Stopwatch();
+
+        int[] array = new int[] { -10, 2, 7, 13, 81, 99, 25, 12, 42 };
+
+        // Шейкерная сортировка
+		Console.WriteLine("Изначальный массив ");
+        foreach (int i in array)
+        {
+            Console.WriteLine($"{i}");
+        }
 		
-		foreach (int r in list)
-		{
-			Console.Write(r + "\t");
-		}
+        CocktailSort(array);
+        Console.WriteLine("\n");
+		Console.WriteLine("Шейкерная сортировка ");
+        foreach (int i in array)
+        {
+			Console.WriteLine($"{i}");
+        }
+		stopwatch.Stop();
+		Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
 
-		list.RemoveLast();
-		Console.Write("\n");
 
-		foreach (int r in list)
-		{
-			Console.Write(r + "\t");
-		}
+		array = new int[] { -10, 2, 7, 13, 81, 99, 25, 12, 42 };
 
-		list.AddLast(15);
-		Console.Write("\n");
-		
-		foreach (int r in list)
-		{
-			Console.Write(r + "\t");
-		}
+		// Сортировка расческой
+		stopwatch.Start();
+		CombSort(array);
+		Console.WriteLine("\n");
+		Console.WriteLine("Сортировка расческой ");
+		foreach (int i in array)
+        {
+            Console.WriteLine($"{i}");
+        }
+		stopwatch.Stop();
+		Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
+
+
+		/* MyLinkedList<int> list = new MyLinkedList<int>();
+        for (int i = 0; i < 10; i++)
+            list.Add(i);
+        
+        foreach (int r in list)
+        {
+            Console.Write(r + "\t");
+        }
+
+        list.RemoveLast();
+        Console.Write("\n");
+
+        foreach (int r in list)
+        {
+            Console.Write(r + "\t");
+        }
+
+        list.AddLast(15);
+        Console.Write("\n");
+        
+        foreach (int r in list)
+        {
+            Console.Write(r + "\t");
+        } */
 
 		/* MyList<int> list = new MyList<int>();
-		for (int i = 0; i < 10; i++)
-			list.Add(i);
+        for (int i = 0; i < 10; i++)
+            list.Add(i);
 
-		foreach (int r in list)
-		{
-			Console.Write(r);
-		} */
+        foreach (int r in list)
+        {
+            Console.Write(r);
+        } */
 
 		/* int[] array = new int[] { 1, 5, 10, 8, -2, -4 };
         int k = 6;
         First_Method(array, k);
         Second_Method(array, k); */
-
 	}
 
+    // 5 дз
+	// Шейкерная сортировка
+    static void Swap(int[] array, int i, int j)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    static void CocktailSort(int[] inArray)
+    {
+        int left = 0,
+            right = inArray.Length - 1;
+        while (left < right)
+        {
+            for (int i = left; i < right; i++)
+            {
+                if (inArray[i] > inArray[i + 1])
+                    Swap(inArray, i, i + 1);
+            }
+            right--;
+            for (int i = right; i > left; i--)
+            {
+                if (inArray[i - 1] > inArray[i])
+                    Swap(inArray, i - 1, i);
+            }
+            left++;
+        }
+    }
+
+	// Сортировка расческой
+    static int[] CombSort(int[] input)
+    {
+        double gap = input.Length;
+        bool swaps = true;
+        while (gap > 1 || swaps)
+        {
+            gap /= 1.247330950103979;
+            if (gap < 1)
+            {
+                gap = 1;
+            }
+            int i = 0;
+            swaps = false;
+            while (i + gap < input.Length)
+            {
+                int igap = i + (int)gap;
+                if (input[i] > input[igap])
+                {
+                    int swap = input[i];
+                    input[i] = input[igap];
+                    input[igap] = swap;
+                    swaps = true;
+                }
+                i++;
+            }
+        }
+        return input;
+    }
+
+    // 2 дз
     static void First_Method(int[] array, int k)
     {
         Console.Write("First_Method: ");
